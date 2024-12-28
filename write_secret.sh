ab353280f51f0606d4d4cfe00e66446f9b9c786a
ArtemL12 <95000683+ArtemL12@users.noreply.github.com>
2024-12-28 16:21:48 +0200

    Update write_secret.sh

--- write_secret.sh
+++ write_secret.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "My secret value is: ${{ secrets.MY_SECRET }}" > /home/test.txt
-echo "My secret value is: ${{ secrets.MY_SECRET }}" >> /home/test.txt
-echo "My secret value is: ${{ secrets.MY_SECRET }}" >> /home/test.txt
+echo "My secret value is: $MY_SECRET" > /home/test.txt
+echo "My secret value is: $MY_SECRET" >> /home/test.txt
+echo "My secret value is: $MY_SECRET" >> /home/test.txt
 echo "File /home/test.txt created and updated with secrets."
